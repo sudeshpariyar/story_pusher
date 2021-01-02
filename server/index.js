32ef822a469257ece7451656b7e1d460cb4116fc
sudesh pariyar <46883066+sudeshpariyar@users.noreply.github.com>
2021-01-02 22:07:16 +1100

    updated

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
-  "mongodb+srv://sudeshpariyar:Yamuna_Pariyar_08@cluster0.cfgsx.mongodb.net/<dbname>?retryWrites=true&w=majority";
+  "mongodb+srv://sudeshpariyar:******************@cluster0.cfgsx.mongodb.net/<dbname>?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
